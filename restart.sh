3c0450f257f552516744130781b8215f112acc56
Daniel Wang <haidixiansheng@126.com>
2021-08-09 14:06:24 +0800

    v 1.0.1
    加上选项功能，可以实现多个tomcat的管理

--- restart.sh
+++ restart.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 #
-#作者：Daniel
-#日期：2021年8月8日
-#脚本描述：Tomcat程序实现脚本自动重启
+#Auther：Daniel
+#Date&Time：2021年8月9日
+#Discription：实现通过脚本自动重启tomcat
 
 #定义Tomcat的安装目录，需要根据自己的部署位置调整
-echo "Tomcat is shuting..."
-TOMCATPATH=/opt/webserver/Tomcat-8.5.63
+echo "Hello,please choose your tomcat to restart:"
+read -p "Your choice(ht,gfzq,waibao,tuoguan,zx,hx,zs,zyjj,xcgf,dsq,Tomcat):" TOMCATVERSION
+case $TOMCATVERSION in
+	ht)		TOMCATPATH=/opt/webserver/tomcat8-hthof;;
+	gfzq)		TOMCATPATH=/opt/webserver/tomcat8-gfzqhof;;
+	waibao)		TOMCATPATH=/opt/webserver/tomcat8-waibao;;
+	tuoguan)	TOMCATPATH=/opt/webserver/tomcat8-tuoguan;;
+	zx)		TOMCATPATH=/opt/webserver/tomcat8-zxhof;;
+	hx)		TOMCATPATH=/opt/webserver/tomcat-hxhof;;
+	zs)		TOMCATPATH=/opt/webserver/tomcat8-zsfusion;;
+	zyjj)		TOMCATPATH=/opt/webserver/tomcat8-zyjjfusion;;
+	xcgf)		TOMCATPATH=/home/xbrl/tomcat8-xcgf;;
+	dsq)		TOMCATPATH=/home/xbrl/tomcat8-dsq;;
+	Tomcat)		TOMCATPATH=/opt/webserver/Tomcat-8.5.63;;
+esac
 
 #关闭对应的tomcat
-ps -ef | grep tomcat | grep -v grep | awk '{print $2}' | sed -e "s/^/kill -9 /g" | sh -
+echo "$TOMCATVERSION is shuting..."
+$TOMCATPATH/bin/shutdown.sh &>/dev/null
+sleep 3
+ps -ef | grep $TOMCATVERSION | grep -v grep | awk '{print $2}' | sed -e "s/^/kill -9 /g" | sh -
 sleep 1
 
-#启动tomcat
-echo "Tomcat is shut successfully,tomcat starting..." 
-$TOMCATPATH/bin/startup.sh
-#判断tomcat是否正常启动,并查看对应日志
-tail -f /opt/webserver/Tomcat-8.5.63/logs/catalina.out
-
+#启动tomcat,并查看对应日志
+echo "$TOMCATVERSION shut successfully,$TOMCATVERSION is starting..." 
+sleep 1
+$TOMCATPATH/bin/startup.sh &>/dev/null
+tail -f $TOMCATPATH/logs/catalina.out
