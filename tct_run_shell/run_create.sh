24f381cee9bbd6f956fc8270c9f65f146aab904b
Daniel Wang <danielw7@163.com>
2022-01-06 16:42:11 +0800

    2.6.3

--- tct_run_shell/run_create.sh
+++ tct_run_shell/run_create.sh
@@ -1,11 +1,7 @@
 #bin/bash
 
 VERSION=2.6.3
-#更新程序，将其部署到run文件
+#更新程序，将其加载到run文件
 zip -r tctconfig.zip tctconfig
 cat tct_run.sh tctconfig.zip > tctconfig-$VERSION.run
-#更新程序内部的run文件
-cp tctconfig-$VERSION.run tctconfig
-zip -r tctconfig.zip tctconfig
-cat tct_run.sh tctconfig.zip > tctconfig-$VERSION.run
-
+rm -rf tctconfig.zip
